7f8af6cff6279b25fe7035b2144e67c92eb397a1
Tom C <matatopotato@gmail.com>
2024-09-25 21:28:55 -0300

    class c structs

---
+++ TrabajoGrupal01/tomas/03_pointers/pointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+
+
+    // crear objeto
+
+
+
+
+
+    // puntero del objeto
+
+
+
+    return 0;
+}
--- TrabajoGrupal01/tomas/Timus/1409.c
+++ TrabajoGrupal01/tomas/Timus/1409.c
@@ -1,8 +1,50 @@
 //Two gangsters
 
 #include <stdio.h>
+#include <string.h>
 
+typedef struct{
+	char name[20];
+	int cansShot;
+	int cansToShoot;
+}Gangster;
 
 int main(){
+
+// creando gangster 1
+Gangster gang1;
+strcpy(gang1.name, "Harry");
+
+
+// creando gangster 2
+Gangster gang2;
+
+gang2.name[0] = 'L';
+gang2.name[1] = 'a';
+gang2.name[2] = 'r';
+gang2.name[3] = 'r';
+gang2.name[4] = 'y';
+gang2.name[4] = '\0';
+
+
+
+// escanear el input
+scanf("%d",&gang1.cansShot);
+
+scanf("%d",&gang2.cansShot);
+	
+
+// calcular cuantas latas no disparo cada uno
+
+gang1.cansToShoot= gang2.cansShot-1;
+gang2.cansToShoot= gang1.cansShot-1;
+
+
+
+//imprimir las latas no disparadas por cada uno
+
+printf("%d %d",gang1.cansToShoot,gang2.cansToShoot);
+
+
 	return 0;
 }
